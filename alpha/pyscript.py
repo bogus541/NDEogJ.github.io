338d4db91f1431a18dd5a4a95a5ecc3a02cd342e
NDEogJ <35947029+NDEogJ@users.noreply.github.com>
2018-11-02 12:56:22 -0500

    .

--- alpha/pyscript.py
+++ alpha/pyscript.py
@@ -41,13 +41,13 @@ def parse():
     try:
         page = doc.query["page"]
         doc["main"].html = "<div class='grid-header'><script></script></div>" \
-                           "function audiojs() {" \
-                           "var div= document.getElementsByTagName('head')[0];" \
+                           "function header() {" \
+                           "var div= document.getElementsByTagName('grid-header')[0];" \
                            "var script= document.createElement('script');" \
                            "script.type= 'text/javascript';" \
-                           "script.src= 'audiojs.js';" \
+                           "script.src= 'header.js';" \
                            "div.appendChild(script);}" \
-                           "audiojs();"
+                           "header();"
     except KeyError:
         page = None
     print(f"Page  -- {page}")
